2966cf7e300bbe3588cf6a0c991d1601b3aa67f9
Samuel Yin (Ciallo~) <wsgddjy@live.com>
2024-05-09 00:33:34 +1000

    add main header.

---
+++ src/components/Navigation/MainHeader.tsx
@@ -0,0 +1,33 @@
+﻿import { NavLink } from "react-router-dom";
+
+export default function MainHeader() {
+  return (
+    <header id={"main-header"}>
+      <h1>ReactMentoring</h1>
+      <nav>
+        <ul>
+          <li>
+            <NavLink
+              to={"/"}
+              className={({ isActive }) => (isActive ? "active" : undefined)}
+              end
+            >
+              Our Mission
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to={"/sessions"}
+              className={({ isActive }) => (isActive ? "active" : undefined)}
+            >
+              Browse Sessions
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to={"/upcoming-sessions"}>Upcoming Sessions</NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,10 +1,13 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet } from "react-router-dom";
+import MainHeader from "../components/Navigation/MainHeader.tsx";
 
 export default function Root() {
   return (
     <>
-      {/* Todo: Add Header */}
-      <Outlet />
+      <MainHeader />
+      <main>
+        <Outlet />
+      </main>
     </>
   );
 }
