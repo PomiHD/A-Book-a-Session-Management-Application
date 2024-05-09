b24aae7989f84bc0b1be731a7c6dc77877b93c2b
Samuel Yin (Ciallo~) <wsgddjy@live.com>
2024-05-09 23:43:39 +1000

    add sessions list.

---
+++ src/components/Sessions/SessionsList.tsx
@@ -0,0 +1,28 @@
+﻿type Session = {
+  id: string;
+  title: string;
+  summary: string;
+  description: string;
+  duration: number;
+  date: string;
+  image: string;
+};
+type SessionsListProps = {
+  sessions: Session[];
+};
+function SessionsList({ sessions }: SessionsListProps) {
+  return (
+    <ul className={"sessions-list"}>
+      {sessions.map((session) => (
+        <li key={session.id}>
+          <img src={`${session.image}`} alt={session.title} />
+          <h2>{session.title}</h2>
+          <p>{session.summary}</p>
+          <button>Learn more</button>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default SessionsList;
