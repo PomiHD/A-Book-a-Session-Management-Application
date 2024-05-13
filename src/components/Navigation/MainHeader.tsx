4ecbe17ff0a7e18177e67d926e30ef1a98f7a18b
Samuel Yin (Ciallo~) <wsgddjy@live.com>
2024-05-13 12:50:09 +1000

    add upcoming sessions

--- src/components/Navigation/MainHeader.tsx
+++ src/components/Navigation/MainHeader.tsx
@@ -1,33 +1,47 @@
 ﻿import { NavLink } from "react-router-dom";
+import UpcomingSessions from "../Sessions/UpcomingSessions.tsx";
+import Button from "../UI/Button.tsx";
+import { useState } from "react";
 
 export default function MainHeader() {
+  const [upcomingSessions, setUpcomingSessions] = useState(false);
+
+  function showUpcomingSessions() {
+    setUpcomingSessions(true);
+  }
+  function hideUpcomingSessions() {
+    setUpcomingSessions(false);
+  }
   return (
-    <header id={"main-header"}>
-      <h1>ReactMentoring</h1>
-      <nav>
-        <ul>
-          <li>
-            <NavLink
-              to={"/"}
-              className={({ isActive }) => (isActive ? "active" : undefined)}
-              end
-            >
-              Our Mission
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to={"/sessions"}
-              className={({ isActive }) => (isActive ? "active" : undefined)}
-            >
-              Browse Sessions
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to={"/upcoming-sessions"}>Upcoming Sessions</NavLink>
-          </li>
-        </ul>
-      </nav>
-    </header>
+    <>
+      {upcomingSessions && <UpcomingSessions onClose={hideUpcomingSessions} />}
+      <header id={"main-header"}>
+        <h1>ReactMentoring</h1>
+        <nav>
+          <ul>
+            <li>
+              <NavLink
+                to={"/"}
+                className={({ isActive }) => (isActive ? "active" : undefined)}
+                end
+              >
+                Our Mission
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to={"/sessions"}
+                className={({ isActive }) => (isActive ? "active" : undefined)}
+              >
+                Browse Sessions
+              </NavLink>
+            </li>
+            <li>
+              <Button onClick={showUpcomingSessions}>Upcoming Sessions</Button>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
   );
 }
---
+++ src/components/Sessions/UpcomingSessions.tsx
@@ -0,0 +1,46 @@
+﻿import Modal, { ModalHandle } from "../UI/Modal.tsx";
+import { useSessionsContext } from "../store/sessions-context.tsx";
+import { ReactNode, useEffect, useRef } from "react";
+import UpcomingSession from "./UpcomingSession.tsx";
+import Button from "../UI/Button.tsx";
+
+type UpcomingSessionsProps = {
+  onClose: () => void; // onClose is accepted to "tell" the parent component that the UpcomingSessions component should be removed from the DOM
+};
+export default function UpcomingSessions({ onClose }: UpcomingSessionsProps) {
+  const modal = useRef<ModalHandle>(null);
+  const { upcomingSessions, deleteSession } = useSessionsContext();
+
+  useEffect(() => {
+    if (modal.current) {
+      modal.current.open();
+    }
+  }, []);
+
+  const upcomingSessionsExist = upcomingSessions.length > 0;
+  let content: ReactNode;
+
+  if (!upcomingSessionsExist) {
+    content = <p>No upcoming sessions</p>;
+  }
+  if (upcomingSessionsExist) {
+    content = (
+      <ul>
+        {upcomingSessions.map((session) => (
+          <li key={session.id}>
+            <UpcomingSession
+              session={session}
+              onCancel={() => deleteSession(session.id)}
+            />
+          </li>
+        ))}
+      </ul>
+    );
+  }
+  return (
+    <Modal ref={modal} onClose={onClose}>
+      {content}
+      <Button onClick={onClose}>Close</Button>
+    </Modal>
+  );
+}
